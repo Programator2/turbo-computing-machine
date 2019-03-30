6aa2f3219ffa506de5fc2a3e4fe4b96cc170f816
Roderik Ploszek <roderik.ploszek@gmail.com>
2019-03-30 20:05:20 +0100

    Add documentation for list.h

--- list.h
+++ list.h
@@ -3,7 +3,18 @@
 
 #include "todo.h"
 
+/**
+ * @brief Inserts item to a linked list.
+ *
+ * Insert item @c new after item @c old.
+ */
 void insert_after(struct todo_item* old, struct todo_item* new);
+
+/**
+ * @brief Inserts item to a linked list.
+ *
+ * Insert item @c new before item @c old.
+ */
 void insert_before(struct todo_item* old, struct todo_item* new);
   
 #endif /* TODO_LIST_H */
