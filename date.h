7999d1168c4b595066054719a49ae2570cb12974
Roderik Ploszek <roderik.ploszek@gmail.com>
2019-03-30 20:01:52 +0100

    Add documentation to date.h

--- date.h
+++ date.h
@@ -7,6 +7,14 @@
 
 #define IS_DATE_INVALID(tm) (tm)->tm_mday == 0
 
+/**
+ * @brief Converts date from a string to a @t tm structure.
+ *
+ * Converts date in a format 2019-04-01 to @c tm structure.
+ *
+ * @param buffer Null-terminated string containing the date.
+ * @param tm Pointer to @c tm structure, which will be filled.
+ */
 int read_date(char* buffer, struct tm* tm);
 
 #endif /* TODO_DATE_H */
